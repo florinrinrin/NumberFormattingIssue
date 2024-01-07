ce98601670c563e960293d560d8fe1155d4c7584
florinrinrin <neagraflorin@yahoo.com>
2024-01-07 13:56:04 +0200

    issue

--- src/numbersExercises/Ex8.java
+++ src/numbersExercises/Ex8.java
@@ -1,2 +1,23 @@
-package numbersExercises;public class Ex8 {
+package numbersExercises;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class Ex8 {
+
+
+
+    public static String formatAndDivide (String money) throws ParseException {
+    NumberFormat numberFormat = NumberFormat.getCurrencyInstance(); //same goes if I use the param LOCALE.US
+    Number value = numberFormat.parse(money);
+    BigDecimal result = new BigDecimal(money).divide(new BigDecimal("32.19"), MathContext.DECIMAL32);
+    return numberFormat.format(result);
+}
+
+    public static void main(String[] args) throws ParseException {
+        System.out.println(formatAndDivide("$12,345.93"));
+    }
 }
